008b51d56bcb71c71ba1f798aa1dd14ee78b5a89
Alex <alecxandroch@gmail.com>
2024-12-17 15:13:17 -0300

    fix database

--- functions/auth.js
+++ functions/auth.js
@@ -11,10 +11,7 @@ const ALLOWED_ORIGINS = [
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
